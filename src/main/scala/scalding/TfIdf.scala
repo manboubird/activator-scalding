6dbaebed18c68b6958b22e4c21e1e9ca80cf20ca
Dean Wampler <dean@deanwampler.com>
2013-11-12 15:36:15 -0600

    Now the Tf/Idf output has the full Bible book names.

--- src/main/scala/scalding/TfIdf.scala
+++ src/main/scala/scalding/TfIdf.scala
@@ -54,8 +54,106 @@ class TfIdf(args : Args) extends Job(args) {
     "Num", "Oba", "Pe1", "Pe2", "Phi", "Plm", "Pro", "Psa", "Rev", 
     "Rom", "Rut", "Sa1", "Sa2", "Sol", "Th1", "Th2", "Ti1", "Ti2",
     "Tit", "Zac", "Zep")
-  val booksToIndex = books.zipWithIndex.toMap
 
+  // Almost a third of these books are aprocryphal, and hence aren't in the KJV.
+  val bookAbbrevsToNames = Map(
+    "Act" -> "Acts", 
+    "Aes" -> "Additions to Esther", 
+    "Amo" -> "Amos", 
+    "Aza" -> "Prayer of Azariah", 
+    "Bar" -> "Baruch", 
+    "Bel" -> "Bel and the Dragon", 
+    "Bet" -> "Bel and the Dragon Th", 
+    "Ch1" -> "1 Chronicles", 
+    "Ch2" -> "2 Chronicles", 
+    "Co1" -> "1 Corinthians", 
+    "Co2" -> "2 Corinthians", 
+    "Col" -> "Colossians", 
+    "Dan" -> "Daniel", 
+    "Dat" -> "Daniel Th", 
+    "Deu" -> "Deuteronomy",
+    "Ecc" -> "Ecclesiastes", 
+    "Eph" -> "Ephesians", 
+    "Epj" -> "Epistle of Jeremiah", 
+    "Es1" -> "1 Esdras", 
+    "Es2" -> "2 Esdras", 
+    "Est" -> "Esther", 
+    "Exo" -> "Exodus",
+    "Eze" -> "Ezekiel", 
+    "Ezr" -> "Ezra", 
+    "Gal" -> "Galatians", 
+    "Gen" -> "Genesis",
+    "Hab" -> "Habakkuk", 
+    "Hag" -> "Haggai", 
+    "Heb" -> "Hebrews", 
+    "Hos" -> "Hosea", 
+    "Isa" -> "Isaiah", 
+    "Jam" -> "James", 
+    "Jda" -> "Judges A", 
+    "Jdb" -> "Judges B", 
+    "Jde" -> "Jude", 
+    "Jdg" -> "Judges",
+    "Jdt" -> "Judith", 
+    "Jer" -> "Jeremiah", 
+    "Jo1" -> "1 John", 
+    "Jo2" -> "2 John", 
+    "Jo3" -> "3 John", 
+    "Job" -> "Job", 
+    "Joe" -> "Joel", 
+    "Joh" -> "John", 
+    "Jon" -> "Jonah", 
+    "Jos" -> "Joshua",
+    "Jsa" -> "Joshua A", 
+    "Jsb" -> "Joshua B", 
+    "Kg1" -> "1 Kings", 
+    "Kg2" -> "2 Kings", 
+    "Lam" -> "Lamentations", 
+    "Lao" -> "Laodiceans", 
+    "Lev" -> "Leviticus",
+    "Luk" -> "Luke", 
+    "Ma1" -> "1 Macabees", 
+    "Ma2" -> "2 Macabees", 
+    "Ma3" -> "3 Macabees", 
+    "Ma4" -> "4 Macabees", 
+    "Mal" -> "Malachi", 
+    "Man" -> "Prayer of Manasseh", 
+    "Mar" -> "Mark", 
+    "Mat" -> "Matthew", 
+    "Mic" -> "Micah", 
+    "Nah" -> "Nahum", 
+    "Neh" -> "Nehemiah", 
+    "Num" -> "Numbers",
+    "Oba" -> "Obadiah", 
+    "Ode" -> "Odes", 
+    "Pe1" -> "1 Peter", 
+    "Pe2" -> "2 Peter", 
+    "Phi" -> "Philippians", 
+    "Plm" -> "Philemon", 
+    "Pro" -> "Proverbs", 
+    "Psa" -> "Psalms", 
+    "Pss" -> "Psalms of Solomon", 
+    "Rev" -> "Revelation",
+    "Rom" -> "Romans", 
+    "Rut" -> "Ruth", 
+    "Sa1" -> "1 Samuel", 
+    "Sa2" -> "2 Samuel", 
+    "Sir" -> "Sirach", 
+    "Sol" -> "Song of Solomon", 
+    "Sus" -> "Susanna", 
+    "Sut" -> "Susanna Th", 
+    "Th1" -> "1 Thessalonians", 
+    "Th2" -> "2 Thessalonians", 
+    "Ti1" -> "1 Timothy", 
+    "Ti2" -> "2 Timothy", 
+    "Tit" -> "Titus", 
+    "Toa" -> "Tobit BA", 
+    "Tob" -> "Tobias", 
+    "Tos" -> "Tobit S", 
+    "Wis" -> "Wisdom", 
+    "Zac" -> "Zechariah", 
+    "Zep" -> "Zephaniah") 
+
+   val booksToIndex = books.zipWithIndex.toMap
   val byBookWordCount = Csv(args("input"), separator = "|", fields = kjvSchema)
     .read
     .flatMap('text -> 'word) {
@@ -92,11 +190,12 @@ class TfIdf(args : Args) extends Job(args) {
   // Multiply the term frequency with the inverse document frequency
   // and keep the top N words. "hProd" is the Hadamard product, i.e.,
   // multiplying elementwise, rather than row vector times column vector.
-  // Finally, before writing the output, replace the bookId with the name.
+  // Finally, before writing the output, convert the matrix back to a
+  // Cascading pipe and replace the bookId with the full name.
   docWordMatrix.hProd(invDocFreqMat).topRowElems(n)
     .pipeAs(('bookId, 'word, 'frequency))
     .mapTo(('bookId, 'word, 'frequency) -> ('book, 'word, 'frequency)){
-      tri: (Int,String,Double) => (books(tri._1), tri._2, tri._3)
+      tri: (Int,String,Double) => (bookAbbrevsToNames(books(tri._1)), tri._2, tri._3)
     }
     .write(Tsv(args("output")))
 
